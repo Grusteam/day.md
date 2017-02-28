ffa8828e0dec1eb371945ae6b9d070ab5f3a28c6
Grusteam <ub6lgl@gmail.com>
2017-02-28 18:10:00 +0300

    v.0.0.1

--- src/js/main.js
+++ src/js/main.js
@@ -10,37 +10,69 @@ var
 var
 	currentData,
 	localData,
-	cities = {
-		Tiraspol: 617239,
+	localDataDate,
+	currentDate,
+	citiesObj = {
+		Kishinev: 618426,
+		Beltsi: 618605,
 		Bender: 618577,
-		Ribnita: 617486,
+		Vulkanistsi: 617094,
 		Dubasari: 618365,
-	};
+		Kagul: 618456,
+		Orhei: 617638,
+		Ribnita: 617486,
+		Tiraspol: 617239,
+	},
+	citiesArr = [null, 618426, 618605, 618577, 617094, 618365, 618456, 617638, 617486, 617239];
 
 
-firstStep();
+$('[data-cities]').click(function (e) {
+	e.preventDefault();
+	var x = + e.target.dataset.cit;
+	x = citiesArr[x];
+	console.log(x);
+	if (x) {
+		localData = localStorage.getItem(x);
+		if (localData) {
+		secondStep(JSON.parse(localData));
+		} else {
+			console.log('нету');
+			firstStep();
+		}
+	}
+});
+
 
 function firstStep() {
-	localData = localStorage.getItem('617239'); // локал сторэдж версия
+	localData = localStorage.getItem('618426'); // локал сторэдж версия
+	localDataDate = Date.parse(localStorage.getItem('date')); // когда забирали данные ранее
+	currentDate = Date.parse(new Date());
 	if (localData) {
 		console.log('localData');
 		secondStep(JSON.parse(localData));
-	} else {
+	} else if ((currentDate - localDataDate) / 60000 > 12 ) {
 		$.ajax({
-				url: 'http://api.openweathermap.org/data/2.5/forecast/city?id=617239&units=metric&APPID=4fcbb5699c6ac2b65fea058f7c6fd3f7',
+				url: 'http://api.openweathermap.org/data/2.5/forecast/city?id=618426&units=metric&APPID=077dd80b15fedc5b0d726ad229710dca',
 				type: 'GET',
 				dataType: 'json'
 			})
 			.done(function(data) {
-				var recievedData = typeof data === 'string' ? JSON.parse(data) : data;
-				secondStep(recievedData);
-				setLocalStorage(recievedData);
 				console.log('WebData');
+				var recievedData = typeof data === 'string' ? JSON.parse(data) : data;
+				localStorage.setItem('date', new Date());
+				if ('city' in data){
+					setLocalStorage(recievedData);
+					secondStep(recievedData);
+				} else {
+					console.log(data);
+				}
 			})
 			.fail(function() {
 				console.log("error");
 			})
 			.always(function() {});
+	} else {
+		console.log('прошло меньше 12 минут');
 	}
 }
 
@@ -56,12 +88,15 @@ function triWeater(WeaterData) {
 	for (var i = 0; i < 4; i++) {
 		var
 			code = +WeaterData.list[i].weather[0].id,
+			iconCode = WeaterData.list[i].weather[0].icon,
 			temp = WeaterData.list[i].main.temp,
 			time = WeaterData.list[i].dt_txt,
 			windS = WeaterData.list[i].wind.speed,
-			outstr = 'В ' + time + ' будет ' + typeOfWeater(code) + ' погода с температурой ' + temp + ' градусов и ветром скоростью ' + windS + ' м/с.' + '</br>';
-			
+			city = WeaterData.city.name,
+			sowIcon = 'i' + typeOfWeater(code),
+			outstr = 'В ' + city + ' в ' + time + ' будет ' + code + ' погода с температурой ' + temp + ' градусов и ветром скоростью ' + '<div class="i iw"></div>' + ' ' + windS + ' м/с.' + '<img src="http://openweathermap.org/img/w/' + iconCode + '.png" alt="">' + '<div class="i ' + sowIcon +'"></div>' +'</br>';
 		$('.wrapper').append(outstr);
+		// $(sowIcon).show();
 	}
 }
 
@@ -69,16 +104,22 @@ function triWeater(WeaterData) {
 function typeOfWeater(code) {
 	var
 		weatherCondition =
-		code >= 200 && code < 300 ? 'Thunderstorm' :
-		code >= 300 && code < 400 ? 'Drizzle' :
-		code >= 500 && code < 511 ? 'Rain' :
-		code >= 500 && code < 511 ? 'Rain Rain' :
-		code >= 600 && code < 700 ? 'Snow' :
-		code >= 700 && code < 800 ? 'Atmosphere' :
-		code === 800 ? 'Clear' :
-		code > 800 && code < 900 ? 'Clouds' :
-		code >= 900 && code < 906 ? 'Extreme' :
-		'unknown';
+		code >= 200 && code < 202? 8 :
+		code >= 202 && code < 210 ? 9 :
+		code >= 210 && code < 300 ? 10 :
+		code >= 300 && code < 400 ? 6 :
+		code >= 500 && code < 511 ? 5 :
+		code >= 511 && code < 600? 7 :
+		code >= 600 && code < 602 ? 11 :
+		code >= 602 && code < 612 ? 12 :
+		code >= 612 && code < 700 ? 13 :
+		code >= 700 && code < 800 ? 3 :
+		code === 800 ? 1 :
+		code === 801 ? 2 :
+		code === 802 ? 3 :
+		code >= 803 && code < 900 ? 4 :
+		code >= 900 && code < 906 ? 10 :
+		3;
 	return weatherCondition;
 }
 
@@ -86,18 +127,21 @@ function typeOfWeater(code) {
 // запись в localStorage
 function setLocalStorage(recievedData) {
 	var toLocalStorage = JSON.stringify(recievedData);
-	localStorage.setItem('617239', toLocalStorage);
-	localStorage.setItem('date', new Date());
-	sendData();
+	localStorage.setItem('618426', toLocalStorage);
+	// sendData(toLocalStorage);
 }
 
 // ajax отправка данных через localhost
-function sendData() {
-	$.ajax('/api', {
-		data: JSON.stringify(currentData),
-		type: 'POST',
-		success: function(responseData) {
-			console.log(responseData);
-		}
-	});
-}
+// function sendData(data) {
+// 	$.ajax('/api', {
+// 		data: data,
+// 		type: 'POST',
+// 		success: function(responseData) {
+// 			console.log(responseData);
+// 		}
+// 	});
+// }
+// 
+// ключи
+// 4fcbb5699c6ac2b65fea058f7c6fd3f7
+// 077dd80b15fedc5b0d726ad229710dca
